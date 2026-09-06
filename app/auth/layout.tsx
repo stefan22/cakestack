8e4938c4c9d55fef0e1066f9e760eb87289985e8
Stefano L <stefanosemail@proton.me>
2026-09-06 08:09:07 +0100

    amends to cover hover, split txt & back nav all&ok

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -3,18 +3,20 @@ import { buttonVariants } from '@/components/ui/button';
 import Link from 'next/link';
 import { ArrowLeft } from 'lucide-react';
 import { cn } from '@/lib/utils';
+import { BlurFade } from '@/components/motion/blur-fade';
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="min-h-screen mb-0 w-full flex items-center justify-center">
+    <div className="relative min-h-screen mb-0 w-full flex items-center justify-center">
       <div className="absolute top-5 left-5">
         <Link href="/" className={cn(buttonVariants({ variant: 'secondary' }))}>
           <ArrowLeft className="size-4" />
           Go Back
         </Link>
       </div>
-
-      <div className="w-full md:mx-auto">{children}</div>
+      <BlurFade delay={0.14} yOffset={0} blur="1px" className="flex">
+        <div className="w-full md:mx-auto">{children}</div>
+      </BlurFade>
     </div>
   );
 }
